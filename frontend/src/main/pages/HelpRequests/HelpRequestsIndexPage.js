8d0529f05da79b97280d730f002766245b7fa3f7
Harvard Wang <harvardwang@ucsb.edu>
2022-11-03 18:08:29 -0700

    added helprequestindexpage to app.js

--- frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
+++ frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
@@ -1,28 +1,13 @@
-import React from 'react'
-import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
-
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import UCSBDatesTable from 'main/components/UCSBDates/UCSBDatesTable';
-import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
-export default function HelpRequestsIndexPage() {
-
-  const currentUser = useCurrentUser();
-
-  const { data: dates, error: _error, status: _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/helprequests/all"],
-            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/helprequests/all" },
-      []
-    );
-
+export default function TodosIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>HelpRequests</h1>
-            {/* <UCSBDatesTable dates={dates} currentUser={currentUser} /> */}
+        <h1>Todos</h1>
+        <p>
+          This is where the helprequests list page will go
+        </p>
       </div>
     </BasicLayout>
   )
