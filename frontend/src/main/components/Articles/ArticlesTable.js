de3a847c8c731cfa584aeff0edd6c832bf17c6c9
Josiah Ross <josiahnross@gmail.com>
2022-11-08 15:38:27 -0800

    Josiah - Added Articles Fixtures, Tables (with Delete Column), and Table Test

---
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -0,0 +1,70 @@
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { hasRole } from "main/utils/currentUser";
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/article",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
+
+export default function ArticlesTable({ articles, currentUser }) {
+
+    // // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/article/all"]
+    );
+    // // Stryker enable all 
+
+    // // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'Id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
+        {
+            Header: 'Title',
+            accessor: 'title',
+        },
+        {
+            Header: 'Url',
+            accessor: 'url',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Date Added',
+            accessor: 'dateAdded',
+        }
+    ];
+
+    const testid = "ArticlesTable";
+
+    const columnsIfAdmin = [
+        ...columns,
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={articles}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
