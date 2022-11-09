d1ffd5795f6d32d5b16ae562c414c76c018262fc
Josiah Ross <josiahnross@gmail.com>
2022-11-08 16:35:53 -0800

    Josiah - Minor Fix To Merge Conflict

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -79,6 +79,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                 hasRole(currentUser, "ROLE_USER") && (
                   <NavDropdown title="Articles" id="appnavbar-articles-dropdown" data-testid="appnavbar-articles-dropdown" >
                     <NavDropdown.Item as={Link} to="/articles/list" data-testid="appnavbar-articles-list">List Articles</NavDropdown.Item>
+                  </NavDropdown>
                 )
               }
               {
