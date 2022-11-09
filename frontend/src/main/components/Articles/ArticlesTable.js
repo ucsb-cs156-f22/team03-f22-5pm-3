647ab3ada85b633cba50dabcf491bed9ed9e0044
Josiah Ross <josiahnross@gmail.com>
2022-11-09 13:24:27 -0800

    Josiah - Added Article Table Delete Column

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -1,6 +1,30 @@
-import OurTable from "main/components/OurTable";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { hasRole } from "main/utils/currentUser";
 
-export default function ArticlesTable({ articles, _currentUser }) {
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/article",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
+
+export default function ArticlesTable({ articles, currentUser }) {
+
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/article/all"]
+    );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -31,9 +55,16 @@ export default function ArticlesTable({ articles, _currentUser }) {
 
     const testid = "ArticlesTable";
 
+    const columnsIfAdmin = [
+        ...columns,
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
     return <OurTable
         data={articles}
-        columns={columns}
+        columns={columnsToDisplay}
         testid={testid}
     />;
 };
