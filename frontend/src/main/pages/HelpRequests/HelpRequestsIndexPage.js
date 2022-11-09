eace501fb1596dc6a7a992fbb5c36b6ee693d617
Harvard Wang <harvardwang@ucsb.edu>
2022-11-08 17:16:01 -0800

    added helprequestIndexPage test and fixed some text

--- frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
+++ frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
@@ -1,10 +1,10 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function TodosIndexPage() {
+export default function HelpRequestsIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Todos</h1>
+        <h1>Help Requests</h1>
         <p>
           This is where the helprequests list page will go
         </p>
