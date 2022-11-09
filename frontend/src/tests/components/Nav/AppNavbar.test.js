138eb01e5da6f04c2ffdbf94bcd7b7fdd3e4a900
Colter Sirlin <csirlin@ucsb.edu>
2022-11-08 18:14:55 -0800

    cs - removed create from navbar dropdown for MenuItem

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -281,7 +281,6 @@ describe("AppNavbar tests", () => {
         const aElement = dropdown.querySelector("a");
         expect(aElement).toBeInTheDocument();
         aElement?.click();
-        await waitFor( () => expect(getByTestId(/appnavbar-ucsbdiningcommonsmenuitem-create/)).toBeInTheDocument() );
 
     });
    
