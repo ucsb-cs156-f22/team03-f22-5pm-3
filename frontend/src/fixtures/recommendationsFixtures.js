0e049ee3373b9c3c1d57ff6d46db5b607f3a5fde
nick-pellegrin <npellegrin@ucsb.edu>
2022-11-09 17:54:30 -0800

    added rec fixtures

---
+++ frontend/src/fixtures/recommendationsFixtures.js
@@ -0,0 +1,39 @@
+const diningCommonsFixtures = {
+    oneDiningCommons: {
+        "requesterEmail": "Student_A",
+        "preofessorEmail": "Professor_A",
+        "explanation": "Grad School A",
+        "dateRequested": "2000-10-31T01:30:00",
+        "dateNeeded": "2001-10-31T01:30:00",
+        "done": false,
+    },
+    threeCommons: [
+        {
+            "requesterEmail": "Student_B",
+            "preofessorEmail": "Professor_B",
+            "explanation": "Grad School B",
+            "dateRequested": "2000-10-31T01:30:00",
+            "dateNeeded": "2001-10-31T01:30:00",
+            "done": false,
+        },
+        {
+            "requesterEmail": "Student_C",
+            "preofessorEmail": "Professor_C",
+            "explanation": "Grad School C",
+            "dateRequested": "2000-10-31T01:30:00",
+            "dateNeeded": "2001-10-31T01:30:00",
+            "done": false,
+        },
+        {
+            "requesterEmail": "Student_D",
+            "preofessorEmail": "Professor_D",
+            "explanation": "Grad School D",
+            "dateRequested": "2000-10-31T01:30:00",
+            "dateNeeded": "2001-10-31T01:30:00",
+            "done": false,
+        } 
+    ]
+};
+
+
+export { diningCommonsFixtures };
