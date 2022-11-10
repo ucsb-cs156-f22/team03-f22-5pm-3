a2abf772a7e11e6276cfcddfaea1d62f521b6032
Anmol Kapoor <akapoor202@ucsb.edu>
2022-11-10 10:37:19 -0800

    ak - adding stories to component

---
+++ frontend/src/stories/components/UCSBOrganization/UCSBOrganizationTable.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import UCSBOrganizationTable from "main/components/UCSBOrganization/UCSBOrganizationTable";
+import { ucsbOrganizationFixtures } from 'fixtures/ucsbOrganizationFixtures';
+import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+
+export default {
+    title: 'components/UCSBOrganization/UCSBOrganizationTable',
+    component: UCSBOrganizationTable
+};
+
+const Template = (args) => {
+    return (
+        <UCSBOrganizationTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    articles: []
+};
+
+export const threeUCSBOrganization = Template.bind({});
+
+ThreeUCSBOrganization.args = {
+    ucsbOrganization: ucsbOrganizationFixtures.threeUCSBOrganization
+};
+
+export const ThreeUCSBOrganizationAsAdmin = Template.bind({});
+
+ThreeUCSBOrganizationAsAdmin.args = {
+    ucsbOrganization: ucsbOrganizationFixtures.threeUCSBOrganization,
+    currentUser: currentUserFixtures.adminUser
+};
