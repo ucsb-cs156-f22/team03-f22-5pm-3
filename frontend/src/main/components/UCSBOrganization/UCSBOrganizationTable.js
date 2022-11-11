9cd441f3c32ccc51d4702a2db513c7376a3fadd4
Anmol Kapoor <akapoor202@ucsb.edu>
2022-11-10 22:53:46 -0800

    ak - adding delete column and tests

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -1,6 +1,32 @@
-import OurTable from "main/components/OurTable";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils";
+import { hasRole } from "main/utils/currentUser";
 
-export default function UCSBOrganizationTable({ ucsborganization, _currentUser }) {
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/UCSBOrganization",
+        method: "DELETE",
+        params: {
+            orgCode: cell.row.values.orgCode
+        }
+    }
+}
+
+export default function UCSBOrganizationTable({ ucsborganization, currentUser }) {
+
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/UCSBOrganization/all"],
+    )
+    // Stryker enable all
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => {
+        deleteMutation.mutate(cell);
+    }
 
     const columns = [
         {
@@ -21,9 +47,18 @@ export default function UCSBOrganizationTable({ ucsborganization, _currentUser }
         }
     ];
 
+    const testId = "UCSBOrganizationTable";
+
+    const columnsIfAdmin = [
+        ...columns,
+        ButtonColumn("Delete", "danger", deleteCallback, testId)
+    ];
+
+    const display = hasRole(currentUser, "ROLE_USER") ? columnsIfAdmin : columns;
+
     return <OurTable
         data={ucsborganization}
-        columns={columns}
-        testid={"UCSBOrganizationTable"}
+        columns={display}
+        testid={testId}
     />;
 };
