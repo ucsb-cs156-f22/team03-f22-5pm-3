2366cc2b6eabb1aa3b3e147626c4174449351e7d
nick-pellegrin <npellegrin@ucsb.edu>
2022-11-11 16:06:18 -0800

    fixed table

--- frontend/src/main/components/Recommendations/RecommendationsTable.js
+++ frontend/src/main/components/Recommendations/RecommendationsTable.js
@@ -1,23 +1,15 @@
 import OurTable from "main/components/OurTable";
 // import OurTable, { ButtonColumn} from "main/components/OurTable";
-import { useBackendMutation } from "main/utils/useBackend";
-import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useBackendMutation } from "main/utils/useBackend";
+// import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
 // import { useNavigate } from "react-router-dom";
 // import { hasRole } from "main/utils/currentUser";
 
 
 export default function RecommendationsTable({ recommendations }) {
 
-    // Stryker disable all : hard to test for query caching
-    const deleteMutation = useBackendMutation(
-        cellToAxiosParamsDelete,
-        { onSuccess: onDeleteSuccess },
-        ["/api/ucsbdiningcommons/all"]
-    );
-    // Stryker enable all 
 
-    // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    
 
     const columns = [
         {
