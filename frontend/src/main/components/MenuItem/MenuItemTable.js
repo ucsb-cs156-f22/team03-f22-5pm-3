05aa7761a85e8c056a9d000c09039438cffdd01a
csirlin <csirlin@ucsb.edu>
2022-11-12 17:18:33 -0800

    cs - modified MenuItemTable.js to include a delete column visible to admin user

--- frontend/src/main/components/MenuItem/MenuItemTable.js
+++ frontend/src/main/components/MenuItem/MenuItemTable.js
@@ -1,7 +1,30 @@
-import OurTable from "main/components/OurTable";
-import { _hasRole } from "main/utils/currentUser"
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { hasRole } from "main/utils/currentUser"
 
-export default function MenuItemTable({ menuItems, _currentUser }) {
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/ucsbdiningcommonsmenuitem",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
+
+export default function MenuItemTable({ menuItems, currentUser }) {
+
+	// Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/ucsbdiningcommonsmenuitem/all"]
+    );
+    // Stryker enable all 
+
+	// Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -21,12 +44,20 @@ export default function MenuItemTable({ menuItems, _currentUser }) {
             accessor: 'station',
         }
     ];
-
+	
 	const testid = "MenuItemTable";
+
+	const columnsIfAdmin = [
+        ...columns,
+    	ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+	
 
     return <OurTable
         data={menuItems}
-        columns={columns}
+        columns={columnsToDisplay}
         testid={testid}
     />;
 };
